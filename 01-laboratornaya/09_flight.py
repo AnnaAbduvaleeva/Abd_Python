9abe682abde3060001142a875c56ee688d508d77
AnnaAbduvaleeva <annushka908@mail.ru>
2022-03-29 11:21:06 +0300

    Homework

---
+++ 01-laboratornaya/09_flight.py
@@ -0,0 +1,3 @@
+departure_city = input('Введите город вылета: ')
+arrival_city = input('Введите город прилета: ')
+print(departure_city, '-', arrival_city)
--- 02_laboratornaya/02_report.py
+++ 02_laboratornaya/02_report.py
@@ -3,7 +3,7 @@ fst_quarter = float(input())
 sec_quarter = float(input())
 frd_quarter = float(input())
 fth_quarter = float(input())
-sum1 = fst_quarter + sec_quarter
-sum2 = frd_quarter + fth_quarter
-res = sum1 / sum2
+sum_1 = fst_quarter + sec_quarter
+sum_2 = frd_quarter + fth_quarter
+res = sum_1 / sum_2
 print("%.3f" % res)
